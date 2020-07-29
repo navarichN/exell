76bdc771c9e623aa6c7fb10a6e09d7e5b36699c0
navarichN <54509830+navarichN@users.noreply.github.com>
2020-07-29 19:49:41 +0300

    feat: add framework functional

---
+++ src/core/utils.js
@@ -0,0 +1,7 @@
+//Этот концепт наз-ся 'Pure functions'
+export function capitalize(string) {
+  if (typeof string !== 'string') {
+    return ''
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
